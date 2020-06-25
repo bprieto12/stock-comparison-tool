ee827f1e9feecf78fe43aa1ce4d8cc55272049da
Brenden Prieto <12bprieto@gmail.com>
2020-06-25 13:50:54 -0700

    had to include process.env.PORT to server because heroku dynamically allocates the port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 7000;
+const port = process.env.PORT || 7000;
 const axios = require("axios");
 const path = require('path');
 
